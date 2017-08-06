4ec5c910fb3d945ec716adc7b74efc9681da6d61
harry oslislo <oslislo1@hotmail.com>
2017-08-06 10:20:45 -0400

    Initial commit

---
+++ percentess.py
@@ -0,0 +1,6 @@
+# The %s is known as a format specifier, specifically for strings. Specifiers act as a
+# placeholder; after the string, the % precedes the values to be inserted into the format
+# specifier.
+print "John is %s%s" % ("every ", "Man")
+# newer functionality
+print "John is {}{}".format("every ", "Man")
